b2bc5d8fdafe7285968afad39a65c601fb6c5b36
Nicolas Bock <nicolasbock@gmail.com>
2019-01-23 06:13:54 -0700

    Add player in part 4
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- game.js
+++ game.js
@@ -20,6 +20,8 @@ var game = new Phaser.Game(config);
 
 var platforms;
 
+var player;
+
 function preload ()
 {
   this.load.image('sky', 'assets/sky.png');
@@ -43,6 +45,31 @@ function create ()
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
 }
 
 function update ()
