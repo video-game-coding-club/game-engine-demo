08002844c594a674804789abd82526c660ac7898
Nicolas Bock <nicolasbock@gmail.com>
2019-01-24 13:36:26 -0700

    Add dynamic resizing of canvas
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- game.js
+++ game.js
@@ -26,6 +26,30 @@ var score = 0;
 var scoreText;
 var bombs;
 
+(function() {
+  let initialize = function() {
+    window.addEventListener('resize', resizeCanvas);
+  };
+
+  let resizeCanvas = function() {
+    let canvas = document.querySelector("canvas");
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let windowRatio = windowWidth / windowHeight;
+    let gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+      canvas.style.width = windowWidth + "px";
+      canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+      canvas.style.width = (windowHeight * gameRatio) + "px";
+      canvas.style.height = windowHeight + "px";
+    }
+  };
+
+  initialize();
+})();
+
 function preload ()
 {
   this.load.image('sky', 'assets/sky.png');
